56b07d60e11d954146739117b094156ff112b390
xuanhop <xuanhopdo9712@gmail.com>
2021-05-13 23:05:44 +0700

    4th

--- js/myscript.js
+++ js/myscript.js
@@ -32,21 +32,27 @@ const api_key = 'ce4a0c92d75ac80f5a83ff2d8a4d8009';
 function callApi(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&lang=vi&units=metric`).then(response => response.json())
         .then(function (data) {
-            var temperature = document.getElementById('temperature');
-            var like = document.querySelector('#like');
-            var min_temp = document.querySelector('#min_temp');
-            var max_temp = document.querySelector('#max_temp');
-            var icon_tt = document.querySelector('#icon-tt');
-            var description = document.querySelector('#description');
-            var wind = document.querySelector('#wind');
+            if (data.cod == 200) {
+                var temperature = document.getElementById('temperature');
+                var like = document.querySelector('#like');
+                var min_temp = document.querySelector('#min_temp');
+                var max_temp = document.querySelector('#max_temp');
+                var icon_tt = document.querySelector('#icon-tt');
+                var description = document.querySelector('#description');
+                var wind = document.querySelector('#wind');
+                var humidity = document.querySelector('#humidity');
 
-            temperature.innerHTML = parseFloat(data.main.temp).toFixed(2);
-            like.innerHTML = parseFloat(data.main.feels_like).toFixed(2);
-            min_temp.innerHTML = parseFloat(data.main.temp_min).toFixed(2);
-            max_temp.innerHTML = parseFloat(data.main.temp_max).toFixed(2);
-            icon_tt.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-            description.innerHTML = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
-            wind.innerHTML = data.wind.speed
+                temperature.innerHTML = parseFloat(data.main.temp).toFixed(2);
+                like.innerHTML = parseFloat(data.main.feels_like).toFixed(2);
+                min_temp.innerHTML = parseFloat(data.main.temp_min).toFixed(2);
+                max_temp.innerHTML = parseFloat(data.main.temp_max).toFixed(2);
+                icon_tt.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+                description.innerHTML = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+                wind.innerHTML = data.wind.speed;
+                humidity.innerHTML = data.main.humidity;
+            } else if (data.cod == 404) {
+                alert('Không tìm thấy thành phố');
+            }
         }).catch(function (err) {
             console.log('fail to call api. Mess:' + err);
         })
